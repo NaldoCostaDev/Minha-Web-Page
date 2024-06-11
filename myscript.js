25ef773ef42ea5bf4cd2b8c1cd4d29cb316049eb
Naldo Costa <claudio_nald95@hotmail.com>
2024-06-11 12:26:05 -0300

    adicionando menu

--- myscript.js
+++ myscript.js
@@ -9,3 +9,31 @@ function playBackgroundMusic() {
 // função de reprodução de música
 document.getElementById('logo').addEventListener('click', playBackgroundMusic); 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const menuMobile = document.querySelector('.menu-mobile');
+    const menuListar = document.querySelector('.menu-lista');
+    const btnFechar = document.querySelector('.btn-fechar');
+    const menuLinks = document.querySelectorAll('.menu-mobile nav ul li a');
+
+    menuListar.addEventListener('click', () => {
+        menuMobile.classList.add('active');
+    });
+
+    document.addEventListener('click', (event) => {
+        const isClickInsideMenu = menuMobile.contains(event.target);
+        const isClickOnHamburguer = menuListar.contains(event.target);
+
+        if (!isClickOnHamburguer && menuMobile.classList.contains('active')) {
+            if (!isClickInsideMenu || event.target.classList.contains('btn-fechar') || event.target.closest('.btn-fechar')) {
+                menuMobile.classList.remove('active');
+            }
+        }
+    });
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuMobile.classList.remove('active');
+        });
+    });
+});
+
