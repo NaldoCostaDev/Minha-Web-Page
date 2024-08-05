445f84742ffa8482558ca7c4af7b5000226dec36
Naldo Costa <claudio_nald95@hotmail.com>
2024-08-05 12:55:32 -0300

    atualizacao responsiva e curriculo

--- myscript.js
+++ myscript.js
@@ -2,12 +2,12 @@
 // Função para reproduzir a música de fundo
 function playBackgroundMusic() {
     // URL da música de fundo
-    var audio = new Audio("audio/13.mp3");
+    var audio = new Audio("audio/audio-portifolio1.mp3");
     audio.play(); // Reproduz a música
 }
 
 // função de reprodução de música
-document.getElementById('logo').addEventListener('click', playBackgroundMusic); 
+document.getElementById('menu').addEventListener('click', playBackgroundMusic); 
 
 document.addEventListener('DOMContentLoaded', () => {
     const menuMobile = document.querySelector('.menu-mobile');
